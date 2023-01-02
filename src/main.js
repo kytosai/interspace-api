e0af357177b88125b0e1f9449828ad6236032089
kytosai <kytosai@gmail.com>
2023-01-02 23:49:22 +0700

    change time delay

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const _ = require('lodash');
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
@@ -17,10 +18,23 @@ server.get('/echo', (req, res) => {
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
+server.use(async (req, res, next) => {
   if (req.method === 'POST') {
     req.body.createdAt = Date.now();
   }
+
+  /*
+    Fake delay api
+  */
+  if (req.url.includes('/products')) {
+    // fake delay
+    await new Promise((res) => {
+      setTimeout(() => {
+        res();
+      }, _.random(200, 600));
+    });
+  }
+
   // Continue to JSON Server router
   next();
 });
